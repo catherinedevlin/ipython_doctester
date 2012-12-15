33699f640eede55c7ee0827af1a0d29ffca369ad
Catherine Devlin <catherine_devlin@kace.com>
2012-12-15 14:39:07 -0500

    oops, update version numbers

--- ipython_doctester.py
+++ ipython_doctester.py
@@ -21,7 +21,7 @@ import sys
 import requests
 import IPython.zmq.displayhook
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 finder = doctest.DocTestFinder()
 docent_url = 'http://ipython-docent.appspot.com'
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ipython_doctester',
     author='Catherine Devlin',
     author_email='catherine.devlin@gmail.com',
-    version='0.1.1',
+    version='0.2.0',
     url='http://pypi.python.org/pypi/ipython_doctester/',
     py_modules = [
         "ipython_doctester",
