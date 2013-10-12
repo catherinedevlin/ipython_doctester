a64068222d74110ac147ec41387b4c93c0cd44c9
Catherine Devlin <catherine.devlin@gmail.com>
2013-10-12 12:26:29 -0400

    Oops, didn't mark version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ipython_doctester',
     author='Catherine Devlin',
     author_email='catherine.devlin@gmail.com',
-    version='0.2.3',
+    version='0.3.0',
     url='http://pypi.python.org/pypi/ipython_doctester/',
     py_modules = [
         "ipython_doctester",
@@ -13,7 +13,7 @@ setup(
     description='Run doctests in individual IPython Notebook cells',
     long_description=open('README.txt').read(),
     install_requires=[
-        "ipython >= 0.13",
+        "ipython >= 1.0",
         "pyzmq >= 2.1.4",
         "requests",
         ]
